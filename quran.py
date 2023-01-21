0ba126dd9ffd8078911c9ec328c89285830dd510
futurevoid <tamersaeed710@gmail.com>
2023-01-22 00:08:55 +0200

    better error handling

--- quran.py
+++ quran.py
@@ -54,62 +54,66 @@ print(f"""
         2.download
         3.download_all      \n\n\n""")
 first_menu=input(">> ")
-if first_menu == "1":
+try:
+    if first_menu == "1":
+        os.system('clear')
+        print("      readers name\n\n")
+        print("[+]top readers:")
+        print("[1] mshary al-afasy [2] maher al muaiqly [3] yaser aldosry \n\n\n")
+        inp=input("choose the reader:")
+
+        if inp == "1":
+            os.system('clear')
+            soura()
+            sora = input(">>")
+            number= str(sora)
+            zero=number.zfill(3)
+            print(zero)
+            subprocess.run(["mplayer",f"https://server8.mp3quran.net/afs/{zero}.mp3"])
+        if inp == "2":
+            os.system('clear')
+            soura()
+            sora = input(">>")
+            number= str(sora)
+            zero=number.zfill(3)
+            print(zero)
+            subprocess.run(["mplayer",f"https://server8.mp3quran.net/maher/{zero}.mp3"])
+
+        if inp == "3":
+            os.system('clear')
+            soura()
+            sora = input(">>")
+            number= str(sora)
+            zero=number.zfill(3)
+            print(zero)
+            subprocess.run(["mplayer",f"https://server11.mp3quran.net/yasser/{zero}.mp3"])
+
+    if first_menu == "2":
+        os.system('clear')
+        print("\t\t\t[+]download")
+        soura()
+        sor=input("which sora:")
+        number= str(sor)
+        zero=number.zfill(3)
+        print(zero)
+        #wget https://server8.mp3quran.net/afs/114.mp3 -P /home/kali/Desktop/quran/downloads
+        #subprocess.run(["wget",f"https://server8.mp3quran.net/afs/{zero}.mp3", "-P /downloads"])
+        os.system(f"wget https://server8.mp3quran.net/afs/{zero}.mp3 -P downloads")
+
+    if first_menu == "3":
+        for number in range(115):
+            number = str(number)
+            zero=number.zfill(3)
+            print(zero)
+            os.system(f"wget https://server7.mp3quran.net/afs/{zero}.mp3 -P downloads")
+
+except KeyboardInterrupt:
     os.system('clear')
-    print("      readers name\n\n")
-    print("[+]top readers:")
-    print("[1] mshary al-afasy [2] maher al muaiqly [3] yaser aldosry \n\n\n")
-    inp=input("choose the reader:")
-
-    if inp == "1":
-        os.system('clear')
-        soura()
-        sora = input(">>")
-        number= str(sora)
-        zero=number.zfill(3)
-        print(zero)
-        subprocess.run(["mplayer",f"https://server8.mp3quran.net/afs/{zero}.mp3"])
-    if inp == "2":
-        os.system('clear')
-        soura()
-        sora = input(">>")
-        number= str(sora)
-        zero=number.zfill(3)
-        print(zero)
-        subprocess.run(["mplayer",f"https://server8.mp3quran.net/maher/{zero}.mp3"])
-
-    if inp == "3":
-        os.system('clear')
-        soura()
-        sora = input(">>")
-        number= str(sora)
-        zero=number.zfill(3)
-        print(zero)
-        subprocess.run(["mplayer",f"https://server11.mp3quran.net/yasser/{zero}.mp3"])
-
-if first_menu == "2":
-    os.system('clear')
-    print("\t\t\t[+]download")
-    soura()
-    sor=input("which sora:")
-    number= str(sor)
-    zero=number.zfill(3)
-    print(zero)
-    #wget https://server8.mp3quran.net/afs/114.mp3 -P /home/kali/Desktop/quran/downloads
-    #subprocess.run(["wget",f"https://server8.mp3quran.net/afs/{zero}.mp3", "-P /downloads"])
-    os.system(f"wget https://server8.mp3quran.net/afs/{zero}.mp3 -P downloads")
-
-if first_menu == "3":
-    for number in range(115):
-        number = str(number)
-        zero=number.zfill(3)
-        print(zero)
-        os.system(f"wget https://server7.mp3quran.net/afs/{zero}.mp3 -P downloads")
+    print("bye")
+    exit(0)
 
 
 
 
-
-
-#import os
-#os.system("ls -l")
+    #import os
+    #os.system("ls -l")

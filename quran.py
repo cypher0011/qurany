d8813e49ec8c84e757d68a7bf7cec545a5a67395
futurevoid <tamersaeed710@gmail.com>
2023-01-22 00:55:50 +0200

    added distro based dependencies installation

--- quran.py
+++ quran.py
@@ -1,23 +1,24 @@
 import os
 import subprocess
-import errno
-from data import *
+import distro
+from data import soura
 
 
-#colors 
+# colors
 nocolor = "\033[0m"
-black   = "\033[0;30m"
-red     = "\033[0;31m"
-green   = "\033[0;32m"
-yellow  = "\033[0;33m"
-white   = "\033[0;37m"
+black = "\033[0;30m"
+red = "\033[0;31m"
+green = "\033[0;32m"
+yellow = "\033[0;33m"
+white = "\033[0;37m"
 
 
-os.system('clear')
-if not os.path.exists('downloads'):
-    os.makedirs("downloads") 
+os.system("clear")
+if not os.path.exists("downloads"):
+    os.makedirs("downloads")
 
-print("""
+print(
+    """
 
                               
                               
@@ -48,72 +49,85 @@ print("""
 
     update: you can listen to some of famous shaiks 
     comming: listen to all shaiks - can open quran from terminal - add the arabic language
-                """)
-print(f"""
+                """
+)
+print(
+    f"""
         1.listen
         2.download
-        3.download_all      \n\n\n""")
+        3.download_all      \n\n\n"""
+)
 try:
-    first_menu=input(">> ")
+
+    if distro.like() == "arch":
+        subprocess.run(["pacman", "-S", "mplayer"])
+    elif distro.like() == "alpine":
+        subprocess.run(["apk", "add", "mplayer"])
+    else:
+        subprocess.run(["apt-get", "install", "mplayer"])
+
+    first_menu = input(">> ")
     if first_menu == "1":
-        os.system('clear')
+        os.system("clear")
         print("      readers name\n\n")
         print("[+]top readers:")
-        print("[1] mshary al-afasy [2] maher al muaiqly [3] yaser aldosry \n\n\n")
-        inp=input("choose the reader:")
+        print("[1] mashary al-afasy [2] maher al muaiqly [3] yaser aldossary \n\n\n")
+        inp = input("choose the reader:")
 
         if inp == "1":
-            os.system('clear')
+            os.system("clear")
             soura()
             sora = input(">>")
-            number= str(sora)
-            zero=number.zfill(3)
+            number = str(sora)
+            zero = number.zfill(3)
             print(zero)
-            subprocess.run(["mplayer",f"https://server8.mp3quran.net/afs/{zero}.mp3"])
+            subprocess.run(["mplayer", f"https://server8.mp3quran.net/afs/{zero}.mp3"])
         if inp == "2":
-            os.system('clear')
+            os.system("clear")
             soura()
             sora = input(">>")
-            number= str(sora)
-            zero=number.zfill(3)
+            number = str(sora)
+            zero = number.zfill(3)
             print(zero)
-            subprocess.run(["mplayer",f"https://server8.mp3quran.net/maher/{zero}.mp3"])
+            subprocess.run(
+                ["mplayer", f"https://server8.mp3quran.net/maher/{zero}.mp3"]
+            )
 
         if inp == "3":
-            os.system('clear')
+            os.system("clear")
             soura()
             sora = input(">>")
-            number= str(sora)
-            zero=number.zfill(3)
+            number = str(sora)
+            zero = number.zfill(3)
             print(zero)
-            subprocess.run(["mplayer",f"https://server11.mp3quran.net/yasser/{zero}.mp3"])
+            subprocess.run(
+                ["mplayer", f"https://server11.mp3quran.net/yasser/{zero}.mp3"]
+            )
 
     if first_menu == "2":
-        os.system('clear')
+        os.system("clear")
         print("\t\t\t[+]download")
         soura()
-        sor=input("which sora:")
-        number= str(sor)
-        zero=number.zfill(3)
+        sor = input("which sora:")
+        number = str(sor)
+        zero = number.zfill(3)
         print(zero)
-        #wget https://server8.mp3quran.net/afs/114.mp3 -P /home/kali/Desktop/quran/downloads
-        #subprocess.run(["wget",f"https://server8.mp3quran.net/afs/{zero}.mp3", "-P /downloads"])
+        # wget https://server8.mp3quran.net/afs/114.mp3 -P /home/kali/Desktop/quran/downloads
+        # subprocess.run(["wget",f"https://server8.mp3quran.net/afs/{zero}.mp3", "-P /downloads"])
         os.system(f"wget https://server8.mp3quran.net/afs/{zero}.mp3 -P downloads")
 
     if first_menu == "3":
         for number in range(115):
             number = str(number)
-            zero=number.zfill(3)
+            zero = number.zfill(3)
             print(zero)
             os.system(f"wget https://server7.mp3quran.net/afs/{zero}.mp3 -P downloads")
 
+
 except KeyboardInterrupt:
-    os.system('clear')
+    os.system("clear")
     print("bye")
     exit(0)
 
-
-
-
-    #import os
-    #os.system("ls -l")
+    # import os
+    # os.system("ls -l")
